ac1c82db4d35fbc30e2271edb7a6f32e334d0e3f
charterla <2330911429eric2@gmail.com>
2020-05-27 22:17:51 +0800

    restart

--- playRoom/playRoom.js
+++ playRoom/playRoom.js
@@ -1,28 +1,26 @@
+var playBoard = document.getElementById("playBoard");
+var context = playBoard.getContext("2d");
 var block = new Array(), bx = 0;
 
-function hexagon(nowx, nowy , addx, addy, radius) {
-        let angle = Math.PI*2/6;
-        beginShape();
-        for(let nowAngle=0;nowAngle<Math.PI*2;nowAngle+=angle) {
+function hexagon(nowx, nowy, addx, addy, radius) {
+        let angle = Math.PI/3;
+        context.beginPath();
+        for(let nowAngle=0; nowAngle<Math.PI*2; nowAngle+=angle) {
             let tx = addx + nowx * radius * 3 + Math.cos(nowAngle) * radius;
             let ty = addy + nowy * radius * 1.75 + Math.sin(nowAngle) * radius;
-            vertex(tx,ty);
+            if(nowAngle==0)context.moveTo(tx, ty);
+            else context.lineTo(tx, ty);
+            context.stroke();
         }
-        endShape(CLOSE);
-        pop();
+        context.closePath();
 }
 
-function setup() {
-    var radius = 5;
-    createCanvas(1580,875);
+window.onload = function (){
+    var radius = 20;
 
     for(let i=0;i<12;i++)
     {
-        for(let j=0;j<13;j++){block[bx] = new Array();hexagon(j, i, radius, radius*0.875, radius);bx++;}
-        for(let j=0;j<13;j++){block[bx] = new Array();hexagon(j, i, radius*5/2, radius*1.75, radius);bx++;}
+        for(let j=0;j<13;j++){hexagon(j, i, radius, radius*0.875, radius);}
+        for(let j=0;j<13;j++){hexagon(j, i, radius*5/2, radius*1.75, radius);}
     }
-}
-
-function draw() {
-    
 }
